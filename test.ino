a46a9a4f24b7300ffc70ed52255d1834cf18df13
Aquarius-mark2 <Aquarius_mark2@yahoo.co.jp>
2023-06-06 17:22:21 +0900

    add test

---
+++ test.ino
@@ -0,0 +1,21 @@
+int RX_PIN = 16;
+int TX_PIN = 17;
+
+void setup() {
+  Serial.begin(115200);//シリアルモニター＆Loraへの送信用
+  Serial2.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);//Loraからの受信用
+//Lora_Serial2のポートレートはデフォルトで9600
+}
+
+void loop() {
+  int data = 0;
+  Serial.println("edit 1");
+  Serial.println("10 Uprint \"from Lora\r\n\"");
+  Serial.println("run");
+  Serial.println("edit 0");
+  delay(3000);
+  while( Serial2.available()){
+    data = Serial2.read();
+    Serial.write(data);
+  }
+}
