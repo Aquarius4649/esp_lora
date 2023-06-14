08a9164fe9f3e94fbb5c2f22ef0e1d886c9ba199
student-ops <gogakuec@gmail.com>
2023-06-14 18:28:58 +0900

    test

--- tests/resource.h
+++ tests/resource.h
@@ -1,13 +1,14 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-const int ReceiveProgramSize = 2;
+const int ReceiveProgramSize = 30;
 const char *ping_program =
-    "hello \r\n"
+    "\r\n"
     "world";
 
 const char *ReceiveProgram[ReceiveProgramSize] = {
     "hello",
+    "from nagasaki",
     "world"};
 
 #endif
--- tests/tests.ino
+++ tests/tests.ino
@@ -1,9 +1,11 @@
 #include "./resource.h"
+#include "ArxTypeTraits.h"
 
 int RX_PIN = 16;
 int TX_PIN = 17;
 
-void setup(){
+void setup()
+{
   Serial.begin(115200);                            // シリアルモニター＆Loraへの送信用
   Serial2.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN); // Loraからの受信用
   // Lora_Serial2のポートレートはデフォルトで9600
@@ -11,23 +13,29 @@ void setup(){
 
 int count = 0;
 
-void PortWrite(String str){
+void PortWrite(String str)
+{
   Serial.println(str);
   delay(100);
 }
 
-void ArrayPortWrite(const char* str[], int num){
-  for(int i = 0; i < num; i++){
+void ArrayPortWrite(const char *str[], int num)
+{
+  for (int i = 0; i < num; i++)
+  {
     PortWrite(String(str[i]));
   }
 }
 
 int data;
-void loop(){
-  ArrayPortWrite(ReceiveProgram, 2);
-  while (Serial2.available()){
-    data = Serial2.read();
-    Serial.write(data);
-  }
+
+void loop()
+{
+  // ArrayPortWrite(ReceiveProgram, 2);
+  // while (Serial2.available())
+  // {
+  //   data = Serial2.read();
+  //   Serial.write(data);
+  // }
   delay(1000);
 }
